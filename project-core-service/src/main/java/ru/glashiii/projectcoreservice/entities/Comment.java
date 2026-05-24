b66a4823a288347328d04d01e606e2975a7478d8
Glashiii <glashyep@gmail.com>
2026-05-24 17:52:01 +0300

    Comment entity

---
+++ project-core-service/src/main/java/ru/glashiii/projectcoreservice/entities/Comment.java
@@ -0,0 +1,37 @@
+package ru.glashiii.projectcoreservice.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long issueId;
+    @Column(nullable = false)
+    private Long projectId;
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String body;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false, updatable = false)
+    private Instant updatedAt;
+
+}
